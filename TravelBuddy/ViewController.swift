a77b6ed67ce8f145bb5b53102d46a037609fb088
Niiaz Khasanov <nkhsnv@Niiazs-MacBook-Air.local>
2025-06-19 19:45:23 +0300

    first commit

---
+++ TravelBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelBuddy
+//
+//  Created by Niiaz Khasanov on 6/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
